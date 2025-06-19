2c1c07b3a35b256e0094e6d1789320cb00fc6fa0
antares-lft <antares.lafitte@csilyon.fr>
2025-06-19 13:38:02 +0200

    Update slim_batch.sh

--- Simulation_script/Low_recombination/slim_batch.sh
+++ Simulation_script/Low_recombination/slim_batch.sh
@@ -4,8 +4,8 @@
 source /beegfs/data/gueguen/monpy/bin/activate
 source /beegfs/home/alafitte/.bash_aliases
 
-dir=/beegfs/home/alafitte/Pop_size/Internship/Script/Free_recombination
-outdir=/beegfs/data/alafitte/Pop_size/Internship/Results/Free_recombination
+dir=/beegfs/home/alafitte/Pop_size/Internship/Script/Low_recombination
+outdir=/beegfs/data/alafitte/Pop_size/Internship/Results/Low_recombination
 
 mkdir -p ${outdir}
 
@@ -14,7 +14,7 @@ do
 mkdir -p ${outdir}/sim_${num}
 echo "#!/bin/bash
 
-#SBATCH --time=7-00:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=1G
 #SBATCH --partition=normal
@@ -29,7 +29,7 @@ hostname
 
 echo "source $HOME/.bashrc\n" > ${outdir}/sim_${num}/slim_${num}.sh
 
-python3  ${dir}/slim_parse.py -S ${dir}/Population_Splitting_Codon_Template.slim -N ${num} -n 1e3 -r 0.5 -m 1e-6 -l 1e3 -t ${dir}/bat_genes_complete_filtered-PhyML_tree.nhx -b 30000 -f ${dir}/AA_Fitness_Landscape" > ${outdir}/sim_${num}/slim_${num}.sh
+python3  ${dir}/slim_parse.py -S ${dir}/Population_Splitting_Codon_Template.slim -N ${num} -n 1e3 -r 1e-6 -m 1e-6 -l 1e3 -t ${dir}/bat_genes_complete_filtered-PhyML_tree.nhx -b 30000 -f ${dir}/AA_Fitness_Landscape" > ${outdir}/sim_${num}/slim_${num}.sh
 
 cd ${outdir}/sim_${num}
 sbatch slim_${num}.sh
